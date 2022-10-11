19c0b9181bb5fe2f594c509f68cd8ae977a8f681
Nezort11 <nezort11@gmail.com>
2022-10-11 18:31:09 +0300

    Add basic echo func

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { Telegraf } from "telegraf";
 
 console.log("Hello, World!");
@@ -8,6 +9,21 @@ const main = () => {
   }
 
   const bot = new Telegraf(process.env.API_TOKEN);
+
+  bot.start((ctx) => ctx.reply("Welcome to the app!"));
+
+  bot.on("text", async (ctx) => {
+    await ctx.telegram.sendMessage(
+      ctx.chat.id,
+      `You said "${ctx.message.text}"?`
+    );
+  });
+
+  bot.launch();
+  console.log("Bot started!");
+
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
 };
 
 main();
